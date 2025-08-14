eb76d12db19df2b7d38363a27f08256481de95b5
Aditya Sinha <sinha.aditya.btech2024@sitpune.edu.in>
2025-08-14 00:51:38 -0400

    Experiment 11E

---
+++ pvt-public-vars-in-class.cpp
@@ -0,0 +1,36 @@
+// Name: Aditya Sinha
+// PRN: 24070123006
+//Batch: EnTc A1
+//Experiment: 11E
+
+#include <iostream>
+using namespace std; 
+
+class cube{
+    private :
+    double height = 2.0; 
+    double width = 3.0; 
+    double length = 5.0; 
+    
+    public :
+    double volume(){
+        double v; 
+        v = height*width* length ; 
+        return v;
+    }
+
+};
+int main() {
+    cube cu;
+    cout << "The volume is : " << cu.volume();
+
+    return 0;
+}
+
+/*
+OUTPUT :
+
+The volume is : 30
+
+=== Code Execution Successful ===
+*/
